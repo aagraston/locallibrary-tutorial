efb5a2076a29a4e8ec78d586b7eeaa12f03f4184
aagraston <aagraston@gmail.com>
2022-06-10 14:33:43 -0700

    connect to database in app.js

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://m001-student:zBm32zBl@cluster0.itw9d.mongodb.net/local_library?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
